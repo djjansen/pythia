8fe04eb690d45c8b4b616075f0742f2341a1374e
Drew <ajjansen1818@gmail.com>
2021-12-29 00:02:21 -0500

    generalized api function a bit

--- imx_api.py
+++ imx_api.py
@@ -2,7 +2,7 @@ import requests
 import json
 import pandas as pd
 
-url = "https://api.x.immutable.com/v1/assets?page_size=1000&user={{wallet}}&sell_orders=true&order_by=name&direction=asc"
+base_url = "https://api.x.immutable.com/v1"
 # note: cursor at end of response JSON needs to be added to subsequent request to page through results
 
 headers = {"Accept": "application/json"}
@@ -24,6 +24,8 @@ def make_imx_api_call(method, endpoint, headers=headers, payload=None, prior_res
     return total_results
 
 
-df = pd.DataFrame(make_imx_api_call("GET", url))
-df.to_csv('my_cards.csv')
-
+def get_card_collection(wallet):
+    return make_imx_api_call("GET",
+                             f"{base_url}/assets?page_size=1000&user={wallet}"
+                             f"&sell_orders=true&order_by=name&direction=asc")
+    return card_collection

1f7d4c2a140fcd0d6ebdb7200c7d1f2bea2d95ab
djjansen <ajjansen1818@gmail.com>
2021-12-29 23:49:35 -0500

    figured out how to get own match history (API seems to only go back 2 months)

--- gu_api.py
+++ gu_api.py
@@ -2,15 +2,11 @@ import requests
 import json
 
 base_url = 'https://api.godsunchained.com/v0'
-address = '0x3882'
 
+games_won = requests.get(f'{base_url}/match?player_won=XXXXXXX&page=1&perPage=200&start_time=1640311200-1640570399&sort=start_time&order=asc')
 
-#get_inventory = requests.get('https://api.godsunchained.com/v0/user/0xff18298382948028f9d93c4e32be1382204022c8')
+games_won = json.loads(games_won.text)
 
-#print(get_inventory.text)
-
-test = requests.get(f'{base_url}/card?order_by=id&user={{wallet}}')
-
-print(test.text)
+print(games_won)
 
 

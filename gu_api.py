e54a1cf0694ec824be9918f6f34f379012ccbca0
Drew <ajjansen1818@gmail.com>
2022-01-05 23:38:22 -0500

    made generalized function for paging through results

--- gu_api.py
+++ gu_api.py
@@ -2,11 +2,23 @@ import requests
 import json
 
 base_url = 'https://api.godsunchained.com/v0'
-
-games_won = requests.get(f'{base_url}/match?player_won=XXXXXXX&page=1&perPage=200&start_time=1640311200-1640570399&sort=start_time&order=asc')
-
-games_won = json.loads(games_won.text)
-
-print(games_won)
+headers = {"Accept": "application/json"}
 
 
+def make_gu_api_call(method, endpoint, headers=headers, payload=None, prior_results=None, page=1):
+    response = requests.request(method, base_url + endpoint + f'&page={page}', headers=headers)
+    response_json = json.loads(response.text)
+    # start empty result set
+    if prior_results is None:
+        total_results = response_json['records']
+    else:
+        prior_results.extend(response_json['records'])
+        total_results = prior_results
+    # if more results remain, page through
+    if len(total_results) < response_json['total']:
+        make_gu_api_call(method, endpoint, page=page+1, prior_results=total_results)
+    return total_results
+
+
+# card_reference = requests.get(f'{base_url}/proto/1210')
+# card_reference = json.loads(card_reference.text)
